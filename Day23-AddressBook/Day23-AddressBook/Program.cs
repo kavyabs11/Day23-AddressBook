ebafaa008f7c171254d1a549fa5bae6f2baeded3
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-14 20:50:55 +0530

    delete contacts

--- Day23-AddressBook/Day23-AddressBook/Program.cs
+++ Day23-AddressBook/Day23-AddressBook/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 Console.WriteLine("Welcome to Address Book Program in Address Book Main class on Master Branch");
-Console.WriteLine("Press 1 to add contact Press 2 to edit contact");
+Console.WriteLine("Press 1 to add contact Press 2 to edit contact Press 3 to delete contact");
 Address obj = new Address();
 int a = Convert.ToInt32(Console.ReadLine());
 switch (a)
@@ -30,5 +30,12 @@ switch (a)
         string s_name = Console.ReadLine();
         obj.edit(Firstname, s_name);
         obj.display();
+        break;
+    case 3:
+        Console.WriteLine("Enter the first name of the contact you want to delete the details of");
+        string Firstname1 = Console.ReadLine();
+        Console.WriteLine("Enter the second name of the contact you want to delete the details of");
+        string s_name1 = Console.ReadLine();
+        obj.delete(Firstname1, s_name1);
         break;
 }
